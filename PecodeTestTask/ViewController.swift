e0398797772813bc225ad73c8578fa127ef72646
Mariana Piz <marianapiz0412@gmail.com>
2024-06-30 15:46:10 +0300

    Initial Commit

---
+++ PecodeTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PecodeTestTask
+//
+//  Created by Mariana Piz on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
