e6d986649969613256cb039f1c6b76d91c8da1f9
Mariana Piz <marianapiz0412@gmail.com>
2024-07-08 21:19:55 +0300

    Create FirebaseService

---
+++ PecodeTestTask/API/FirebaseService.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseService.swift
+//  PecodeTestTask
+//
+//  Created by Mariana Piz on 08.07.2024.
+//
+
+import Firebase
+
+class FirebaseService {
+    
+    static let shared = FirebaseService()
+        
+    private init() {}
+    
+}
