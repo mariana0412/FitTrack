b875af2b6806c0d76f07644455d27ace9500f348
Mariana Piz <marianapiz0412@gmail.com>
2024-07-20 18:50:08 +0300

    Add profile image to Home screen

--- PecodeTestTask/App/Home/HomeViewController.swift
+++ PecodeTestTask/App/Home/HomeViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 final class HomeViewController: BaseViewController {
     
+    private enum Constants {
+        enum Layout {
+            static let profileImageCornerRadius: CGFloat = 8
+            static let profileImageBorderWidth: CGFloat = 1
+            static let profileImageBorderColor: CGColor = UIColor.primaryYellow.cgColor
+            static let defaultProfileImageName = "profileImage"
+        }
+    }
+    
     var viewModel: HomeViewModel?
     private var superheroName: String?
 
     @IBOutlet private weak var superheroLabel: UILabel!
     @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,6 +55,12 @@ final class HomeViewController: BaseViewController {
         
         nameLabel.text = viewModel.userName
         nameLabel.font = Fonts.sairaRegular16
+        
+        profileImage.clipsToBounds = true
+        profileImage.layer.cornerRadius = Constants.Layout.profileImageCornerRadius
+        profileImage.layer.borderWidth = Constants.Layout.profileImageBorderWidth
+        profileImage.layer.borderColor = Constants.Layout.profileImageBorderColor
+        profileImage.image = UIImage(named: Constants.Layout.defaultProfileImageName)
     
         updateBackgroundImage(named: viewModel.backgroundImageName)
     }
