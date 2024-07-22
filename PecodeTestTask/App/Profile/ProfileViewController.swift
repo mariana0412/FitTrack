7d62b1e479da7fcc4a346b51980aff679f45da5d
Mariana Piz <marianapiz0412@gmail.com>
2024-07-23 00:20:58 +0300

    Add reusable NavigationBarConfigurator

--- PecodeTestTask/App/Profile/ProfileViewController.swift
+++ PecodeTestTask/App/Profile/ProfileViewController.swift
@@ -11,8 +11,6 @@ final class ProfileViewController: BaseViewController {
     
     private enum Constants {
         enum Layout {
-            static let backArrowIcon = "chevron.left"
-            static let backArrowIconSize: CGFloat = 13
             static let instructionNumberOfLines = 2
         }
     }
@@ -38,7 +36,20 @@ final class ProfileViewController: BaseViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        
+        let navigationButtons = NavigationBarConfigurator.configureNavigationBar(
+            for: self,
+            title: ProfileViewModel.Texts.NavigationItem.title,
+            backAction: #selector(backButtonTapped),
+            saveAction: #selector(saveButtonTapped)
+        )
+        
+        if let saveButtonCustomView = navigationButtons.saveButton.customView as? UIButton {
+            saveButton = saveButtonCustomView
+            configureSaveButtonInitialState()
+        }
     }
     
     static func instantiate() -> ProfileViewController {
@@ -47,8 +58,6 @@ final class ProfileViewController: BaseViewController {
     }
     
     private func setupUI() {
-        setupNavigationItem()
-        
         guard let viewModel = viewModel else { return }
         
         updateBackgroundImage(named: viewModel.backgroundImageName)
@@ -71,51 +80,6 @@ final class ProfileViewController: BaseViewController {
         
         addOptionsButton.titleLabel?.text = ProfileViewModel.Texts.addOptionsButton
         addOptionsButton.setupButtonFont(font: Fonts.sairaRegular16, color: .black)
-    }
-    
-    private func setupNavigationItem() {
-        setupNavigationItemTitle()
-        setupNavigationItemLeftBarButton()
-        setupNavigationItemRightBarButton()
-    }
-    
-    private func setupNavigationItemTitle() {
-        let titleLabel = UILabel()
-        titleLabel.text = ProfileViewModel.Texts.NavigationItem.title
-        titleLabel.font = Fonts.sairaMedium18
-        titleLabel.textColor = .primaryWhite
-        navigationItem.titleView = titleLabel
-    }
-    
-    private func setupNavigationItemLeftBarButton() {
-        let backButton = UIButton(type: .system)
-        
-        let chevronLeft = UIImage(systemName: Constants.Layout.backArrowIcon)?.withConfiguration(UIImage.SymbolConfiguration(pointSize: Constants.Layout.backArrowIconSize, weight: .medium))
-        let tintedChevronLeft = chevronLeft?.withTintColor(.primaryYellow, renderingMode: .alwaysOriginal)
-        backButton.setImage(tintedChevronLeft, for: .normal)
-        
-        backButton.setTitle("  \(ProfileViewModel.Texts.NavigationItem.leftBarButton)", for: .normal)
-        backButton.titleLabel?.font = Fonts.sairaMedium18
-        backButton.setTitleColor(.primaryYellow, for: .normal)
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        let backBarButtonItem = UIBarButtonItem(customView: backButton)
-        
-        navigationItem.leftBarButtonItem = backBarButtonItem
-    }
-    
-    private func setupNavigationItemRightBarButton() {
-        let saveButton = UIButton(type: .custom)
-        
-        saveButton.setTitle(ProfileViewModel.Texts.NavigationItem.rightBarButton, for: .normal)
-        saveButton.titleLabel?.font = Fonts.sairaMedium18
-        saveButton.isEnabled = false
-        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
-        let saveBarButtonItem = UIBarButtonItem(customView: saveButton)
-        
-        navigationItem.rightBarButtonItem = saveBarButtonItem
-        
-        self.saveButton = saveButton
-        configureSaveButtonInitialState()
     }
     
     private func configureSaveButtonInitialState() {
