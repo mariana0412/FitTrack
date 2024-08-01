cbe7729547c435f0a035d8bab39d9b9a0b072671
Mariana Piz <marianapiz0412@gmail.com>
2024-08-01 19:05:15 +0300

    Implement basic version of chart

--- PecodeTestTask/App/Chart/ChartViewController.swift
+++ PecodeTestTask/App/Chart/ChartViewController.swift
@@ -20,6 +20,9 @@ final class ChartViewController: BaseViewController {
         
         setupUI()
         bindViewModel()
+        if let optionData = viewModel?.optionData {
+            drawChart(with: optionData)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,6 +62,7 @@ final class ChartViewController: BaseViewController {
         
         titleLabel.text = viewModel.titleText
         subtitleLabel.attributedText = getSubtitleText()
+        subtitleLabel.numberOfLines = 0
     }
     
     private func getSubtitleText() -> NSAttributedString {
@@ -79,5 +83,79 @@ final class ChartViewController: BaseViewController {
         return attributedString
     }
     
+    private func drawChart(with data: OptionData) {
+        guard let maxValue = data.valueArray.compactMap({ $0 }).max() else { return }
+
+        let chartWidth = chartView.bounds.width
+        let chartHeight = chartView.bounds.height
+        let barWidth: CGFloat = 58.0
+        let spacing: CGFloat = 12
+        let maxBarHeight = chartHeight * 0.6
+        let baseLineHeight: CGFloat = 0.5
+        let baseLineYPosition = chartHeight - 40
+
+        let baseLineLayer = CALayer()
+        baseLineLayer.frame = CGRect(x: 0, y: baseLineYPosition, width: chartWidth, height: baseLineHeight)
+        baseLineLayer.backgroundColor = UIColor.primaryWhite.cgColor
+        chartView.layer.addSublayer(baseLineLayer)
+
+        for (index, value) in data.valueArray.enumerated() {
+            guard let value = value else { continue }
+
+            let barHeight = maxBarHeight * CGFloat(value) / CGFloat(maxValue)
+            let xPosition = spacing + CGFloat(index) * (barWidth + spacing)
+            let yPosition = baseLineYPosition - barHeight
+
+            let barLayer = CALayer()
+            barLayer.frame = CGRect(x: xPosition, y: yPosition, width: barWidth, height: barHeight)
+            barLayer.backgroundColor = UIColor.primaryYellow.cgColor
+            barLayer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            barLayer.cornerRadius = barWidth / 4
+
+            chartView.layer.addSublayer(barLayer)
+
+            let valueLabel = UILabel()
+            valueLabel.text = String(format: "%.0f \(data.optionName.metricValue)", value)
+            valueLabel.textColor = .primaryWhite
+            valueLabel.font = Fonts.sairaRegular16
+            valueLabel.sizeToFit()
+            valueLabel.center = CGPoint(x: xPosition + barWidth / 2, y: yPosition - valueLabel.bounds.height / 2 - 4)
+            chartView.addSubview(valueLabel)
+
+            let dateLabel = UILabel()
+            dateLabel.text = formatDate(from: data.dateArray[index])
+            dateLabel.textColor = .primaryWhite
+            dateLabel.font = Fonts.sairaRegular16
+            dateLabel.sizeToFit()
+            dateLabel.center = CGPoint(x: xPosition + barWidth / 2, y: baseLineYPosition + baseLineHeight + dateLabel.bounds.height / 2 + 4)
+            chartView.addSubview(dateLabel)
+
+            if index > 0 {
+                let previousValue = data.valueArray[index - 1] ?? 0
+                let changeValue = value - previousValue
+
+                let changeLabel = UILabel()
+                changeLabel.text = String(format: "%+.0f \(data.optionName.metricValue)", changeValue)
+                changeLabel.textColor = changeValue > 0 ? .lightRed : .lightGreen
+                changeLabel.font = UIFont(name: "Gilroy-SemiBold", size: 14)
+                changeLabel.backgroundColor = changeLabel.textColor
+                changeLabel.textColor = .primaryWhite
+                changeLabel.textAlignment = .center
+                changeLabel.sizeToFit()
+                changeLabel.frame.size = CGSize(width: 56, height: 25)
+                changeLabel.center = CGPoint(x: xPosition + barWidth / 2, y: valueLabel.frame.minY - changeLabel.bounds.height / 2 - 6)
+                changeLabel.layer.cornerRadius = 13
+                changeLabel.layer.masksToBounds = true
+                chartView.addSubview(changeLabel)
+            }
+        }
+    }
     
+    private func formatDate(from timestamp: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM"
+        return dateFormatter.string(from: date)
+    }
+
 }
